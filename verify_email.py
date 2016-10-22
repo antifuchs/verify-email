4c449b66b1e6ab6955af8721b72517d1f7591a44
Andreas Fuchs <asf@boinkor.net>
2016-10-22 11:32:31 -0700

    Output CSV, emit a field "has-sig"
    This should make independent verification of emails a little easier (in
    the first dump zip file, about 1892 emails have no DKIM headers, and of
    those that do, about 1400 fail DKIM validation). Detailed list of
    changes:
    
    * Restructure the python code to use __main__ and a few functions.
    
    * Use csv to generate output: This requires cleaning up the various
      header fields in the output so they don't contain newlines anymore.
    
    * Emit an additional field, has-sig (values True and False) that
      indicates presence of a DKIM signature header.

--- verify_email.py
+++ verify_email.py
@@ -5,47 +5,58 @@ import sys
 import email.parser
 import dateutil.parser
 import dkim
-from openpyxl import Workbook
-from openpyxl.compat import range
+import csv
+import re
 
-wb = Workbook()
-xl_filename = '../verify.xlsx'
-ws = wb.active
-ws.title = "email checks"
-ws.append(['id', 'verified', 'date', 'date text', 'from', 'to', 'subject', 'message-id'])
+header_row = ['id', 'has-sig', 'date', 'date text', 'from', 'to', 'subject', 'message-id', 'verified']
+unpleasant_chars = re.compile("[\n\r]")  # stuff that's not great in CSVs
 
-for filesplit in sorted(map(os.path.splitext, os.listdir(os.getcwd())),key = lambda f: int(f[0])):
-    row = [int(filesplit[0])]
-    filename = "".join(filesplit)
-    print filename
-    f = open(filename, 'r')
-    data = f.read()
-    try:
-        verified = dkim.verify(data)
-        if verified:
-            print "verified"
-            row.append("verified")
-        else:
-            print "failed"
-            row.append("failed")
-    except:
-        print "verify exception"
-        pass
+def emit_csv(filename, email_directory):
+    with open(filename, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
+        writer.writerow(header_row)
+        process_emails(writer, email_directory)
 
-    try:
-        msg = email.message_from_string(data)
-        date = dateutil.parser.parse(msg['date'])
-        meta = [date, msg['date'], msg['from'], msg['to'], msg['subject'], msg['message-id']]
-        row += meta
-    except:
-        print "parse email exception"
-        pass
+def clean_field(s):
+    if isinstance(s, basestring):
+        return unpleasant_chars.sub(' ', s)
+    else:
+        return s
 
-    try:
-        ws.append(row)
-    except:
-        print "worksheet append exception"
-        pass
+def process_emails(writer, email_directory):
+    for filesplit in sorted(map(os.path.splitext, os.listdir(email_directory)),key = lambda f: int(f[0])):
+        row = [int(filesplit[0])]
+        filename = "".join(filesplit)
+        f = open(filename, 'r')
+        data = f.read()
+        has_sig = False
+        try:
+            msg = email.message_from_string(data)
+            date = dateutil.parser.parse(msg['date'])
+            if msg['dkim-signature'] is not None:
+                has_sig = True
+            meta = [has_sig, date, msg['date'], msg['from'], msg['to'], msg['subject'], msg['message-id']]
+            meta = [clean_field(f) for f in meta]
+            row += meta
+        except (RuntimeError, StandardError) as e:
+            print "parse email exception %s" % e
+            row += ['(could not parse)']
+            pass
 
-wb.save(xl_filename)
+        try:
+            verified = dkim.verify(data)
+            if verified and has_sig:
+                row.append("verified")
+            elif has_sig:
+                row.append("failed")
+            else:
+                row.append("unsigned")
+        except (RuntimeError, StandardError, dkim.MessageFormatError) as e:
+            print "DKIM verify exception on message %s: %s" % (filename, e)
+            pass
 
+
+        writer.writerow(row)
+
+if __name__ == '__main__':
+    emit_csv('../verify.csv', os.getcwd())
